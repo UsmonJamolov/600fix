fe1884bcf2e7780bbcc7ac00c4863d4ac6f96f39
usman <usman@example.com>
2026-01-11 23:42:55 +0300

    №11. Admin middleware & product

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	images: {
+		remotePatterns: [{ protocol: 'https', hostname: 'ga5k6wfxt7.ufs.sh', pathname: '**' }],
+	},
+}
 
-export default nextConfig;
+export default nextConfig
